845529246d1aaaa7ba52e491da3d1b8e74641039
ARPIT SHUKLA <47503958+arpitabhi@users.noreply.github.com>
2021-06-07 08:15:22 +0530

    Update serialize.py

--- serialize.py
+++ serialize.py
@@ -1,16 +1,15 @@
-from datetime import datetime
 from typing import Optional
 from pydantic import BaseModel
 
 
 class requestclass(BaseModel):
-   customer_id: int
-   country_code: str
+   customer_id: int = 123
+   country_code: str = "Peru"
    last_order_ts: str = "2018-05-03 00:00:00"
    first_order_ts: str = "2017-05-03 00:00:00"
-   total_orders: int
-   segment_name: str
+   total_orders: int = 15
+   segment_name: str = "recency_segment"
 
 
 class responseclass(BaseModel):
-   vocher_amount: str
+   vocher_amount: str = ""
